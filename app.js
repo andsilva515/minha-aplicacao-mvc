80f3f46d63f03ef3d1f81543a6ce92de0f4ade4d
andsilva515 <anderson.ssilva@outlook.com>
2025-04-28 16:16:03 -0300

    reorganiza adicionar tarefa

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path'); // <<<<<< Adicionado
+const app = express();
+const tarefaController = require('./src/controllers/tarefaController');
+
+// Configurações
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // <<<<<< Adicionado
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Rotas
+app.get('/', tarefaController.exibirTarefas);
+app.get('/adicionar', (req, res) => res.render('adicionarTarefa'));
+app.post('/adicionar', tarefaController.adicionarTarefa);
+app.get('/remover/:id', tarefaController.removerTarefa);
+
+// Inicialização
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
